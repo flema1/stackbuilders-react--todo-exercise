03c688f14497639df1e9d6136fe2b6332f941929
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2018-03-17 21:04:12 -0400

    fix input fieldname

--- client/app/src/components/TodoForm.js
+++ client/app/src/components/TodoForm.js
@@ -1,18 +1,19 @@
 
 import React from 'react';
 
-const TodoForm = ({onInputUpdate, input }) => {
+const TodoForm = ({onInputUpdate, value }) => {
   return (
     <div>
         <form name="todoForm">
             <div className="todo-form-group">
+                <label htmlFor={'email'}>What to do</label>
                 <input 
                     className='todo-form-input' 
                     type="text" 
-                    type="newTodo" 
+                    name="todo" 
                     placeholder='Write something to do...' 
                     onChange={(e)=>onInputUpdate(e)}
-                    value={input}
+                    value={value}
                 />
                 <button className="add-Btn" onClick={()=>console.log('add')}>add</button>
             </div>
