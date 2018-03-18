d4843162f9d50adcbe3941756564b93e71771623
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2018-03-17 22:50:36 -0400

    remove inputfield label

--- client/app/src/components/TodoForm.js
+++ client/app/src/components/TodoForm.js
@@ -2,23 +2,20 @@
 import React from 'react';
 
 const TodoForm = ({onInputUpdate, value, onKeyPress, disabled}) => {
-  console.log(disabled)
   return (
     <div>
-       
-            <div className="todo-form-group">
-                <label htmlFor={'email'}>What to do</label>
-                <input 
-                    className='todo-form-input' 
-                    type="text" 
-                    name="todo" 
-                    placeholder='Write something to do...' 
-                    onChange={(e)=>onInputUpdate(e)}
-                    value={value}
-                    onKeyPress={(e)=>onKeyPress(e)}
-                    disabled = {disabled}
-                />
-            </div>
+      <div className="todo-form-group">
+        <input 
+          className='todo-form-input' 
+          type="text" 
+          name="todo" 
+          placeholder='Write something to do...' 
+          onChange={(e)=>onInputUpdate(e)}
+          value={value}
+          onKeyPress={(e)=>onKeyPress(e)}
+          disabled = {disabled}
+        />
+      </div>
     </div>
   );
 };
