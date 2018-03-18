897c8e8a30d41c3506a25ecf088634533558dc4a
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2018-03-17 21:58:38 -0400

    add errors display

--- client/app/src/components/FormErrors.js
+++ client/app/src/components/FormErrors.js
@@ -4,7 +4,13 @@ import React from 'react';
 const FormErrors = ({ errors }) => {
   return (
     <div>
-        <p>{errors}</p>
+        {Object.keys(errors).map((fieldName, i) => {
+            if (errors[fieldName]!==''){
+                 return (
+                    <p key={i}>{fieldName} { errors[fieldName] }</p>
+                ) 
+            } else return null;       
+         })}
     </div>
   );
 };
