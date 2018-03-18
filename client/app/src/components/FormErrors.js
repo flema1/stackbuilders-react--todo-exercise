a8944e1b0a10e7bbcea80b16c0d8e1c9b5b4a955
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2018-03-18 00:36:29 -0400

    add clear errors icon

--- client/app/src/components/FormErrors.js
+++ client/app/src/components/FormErrors.js
@@ -3,13 +3,13 @@ import React from 'react';
 
 const FormErrors = ({ errors, clear}) => {
   return (
-    <div>
+    <div className={'errors-container'}>
         {Object.keys(errors).map((fieldName, i) => {
             if (errors[fieldName]!==''){
                  return (
-                    <div key={i}> 
-                        <p > { errors[fieldName] }</p>
-                        <p onClick={()=>clear()}>close error</p>
+                    <div className={'errors-section'} key={i}> 
+                        <p> { errors[fieldName] } </p>
+                        <span className={'clear-error'} onClick={()=>clear()}> &#10799; </span>
                     </div>
                 ) 
             }
