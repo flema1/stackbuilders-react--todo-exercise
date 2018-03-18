ac7c8cc219b9b830ab35a6727c811391bdac0445
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2018-03-18 00:37:52 -0400

    add app container

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -5,7 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
      <div className="App" >
-        <Home/>
+       <div className="app-container">
+          <Home/>
+       </div>
       </div>);
   }
 }
